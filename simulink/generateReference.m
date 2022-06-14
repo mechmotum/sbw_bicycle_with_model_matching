63ad0b59b5683494aa6d9d726639e963a85596c4
sdrauksas <71008456+sdrauksas@users.noreply.github.com>
2022-06-14 16:31:41 +0200

    Removed dependency on ACADO
    Now using only qpOASES to solve a linear QP problem

--- simulink/generateReference.m
+++ simulink/generateReference.m
@@ -1,15 +1,14 @@
-function ref = generate_reference(tStart, tEnd, tSample, tChange, ...
+function ref = generateReference(tEnd, tSample, tChange, ...
                                     speed, lWidth)
 % Returns a time-dependent reference trajectory for the double lane-change.
-% The lane change starts 5 seconds after tStart, then the reference stays
+% The lane change starts after 5 seconds, then the reference stays
 % in the second lane for 10 seconds before returning back. How quick the
 % lane change itself is carried out depends on the tChange variable.
 % INPUTS:
-%   tStart - at which time instance to start the reference trajectory
 %   tEnd - at which time instance to stop the reference trajectory. Set the
 %       time to be longer than desired simulation time by the same amount
 %       as the prediction horizon of the MPC controller. Set the tEnd 
-%       smaller than '15 + tStart + tChange' to generate the reference for
+%       smaller than '15 + tChange' to generate the reference for
 %       a single lane change. 
 %   tSample - the sample time of the reference trajectory, must be the same
 %       as the time step of the MPC.
@@ -20,20 +19,20 @@ function ref = generate_reference(tStart, tEnd, tSample, tChange, ...
 %   ref - the generated reference. First column is time, second is
 %   longitudinal distance, third is lateral distance.
 
-    t_ref = (tStart:tSample:tEnd)';
+    t_ref = (0:tSample:tEnd)';
     x_ref = t_ref.*speed;
     y_ref = zeros(length(t_ref),1);
     
     % First lanechange
-    for i = floor((5+tStart)/tSample+1) : floor((5+tStart+tChange)/tSample)
+    for i = floor(5/tSample+1) : floor((5+tChange)/tSample)
         y_ref(i) = y_ref(i-1) + (lWidth*tSample)/tChange;
     end
     % Riding in the second lane
-    for i = floor((5+tStart+tChange)/tSample+1) : floor((15+tStart+tChange)/tSample)
+    for i = floor((5+tChange)/tSample+1) : floor((15+tChange)/tSample)
         y_ref(i) = lWidth;
     end
     % Second lanechange
-    for i = floor((15+tStart+tChange)/tSample+1) : floor((15+tStart+2*tChange)/tSample)
+    for i = floor((15+tChange)/tSample+1) : floor((15+2*tChange)/tSample)
         y_ref(i) = y_ref(i-1) - (lWidth*tSample)/tChange;
         if y_ref(i) < 0
             y_ref(i) = 0;
