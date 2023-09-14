1f4daadce7de231b46cb9e1ea567757f79c5f2d1
kpasma <38067966+kpasma@users.noreply.github.com>
2023-09-14 17:30:34 +0200

    added sil controller to simulate with

--- simulations/simulation.py
+++ simulations/simulation.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib as plt
 import dill
 
-# ##----Define constants
-# # Steer into lean conroller
-# SIL_AVG_SPEED = 6
-# K_SIL_L = 7
-# K_SIL_H = 0.5
+##----Define constants
+# Steer into lean conroller
+SIL_AVG_SPEED = 6
+K_SIL_L = 8
+K_SIL_H = 0.7
 
 ##----Define classes
 # State space system
@@ -62,15 +62,15 @@ class StaticController:
     def __str__(self):
         return f"F:{self.F,}\nG:{self.G}"
 
-# class VariableController:
-#     def __init__(self,gains):
-#         self.F_fun = gains["fb"]
+class VariableController:
+    def __init__(self,fun):
+        self.F_fun = fun
 
-#     def __str__(self):
-#         return f"F:{self.F}"
+    def __str__(self):
+        return f"F:{self.F}"
 
-#     def calc_gain(self, var):
-#         self.F = self.F_fun(var)
+    def calc_gain(self, var):
+        self.F = self.F_fun(var)
         
 
 ##----Define functions
@@ -79,20 +79,21 @@ def calc_bicycle_matrices(bike: VariableStateSpaceSystem, speed):
     inpt = {"A": speed, "B": None}
     bike.calc_mtrx(inpt)
 
-# def sil_gain_fun(speed):
-#     '''
-#     See Schwab et al., 'Some Recent Developments in Bicycle Dynamics and Control', 2008
-#     Dimensions: A-4x4, B-4x2
-#     State vector: [phi, delta, dphi, ddelta],
-#     Input vector: [Tphi, Tdelta]
-#     TODO: remove magic numbers (2,4) and [2][1]?
-#     '''
-#     gain = np.zeros((2,4))
-#     if speed < SIL_AVG_SPEED:
-#         gain[1][2] = K_SIL_L*(SIL_AVG_SPEED - speed) # *dphi
-#     else:
-#         gain[1][0] = K_SIL_H*(speed - SIL_AVG_SPEED) # *phi
-#     return gain
+# gain calculation function for steer into lean controller
+def sil_gain_fun(speed):
+    '''
+    See Schwab et al., 'Some Recent Developments in Bicycle Dynamics and Control', 2008
+    Dimensions: A-4x4, B-4x2
+    State vector: [phi, delta, dphi, ddelta],
+    Input vector: [Tphi, Tdelta]
+    TODO: remove magic numbers (2,4) and [2][1]?
+    '''
+    gain = np.zeros((2,4))
+    if speed < SIL_AVG_SPEED:
+        gain[1][2] = K_SIL_L*(SIL_AVG_SPEED - speed) # *dphi
+    else:
+        gain[1][0] = K_SIL_H*(speed - SIL_AVG_SPEED) # *phi
+    return gain
 
 
 ###---------------------------------[START]---------------------------------###
@@ -111,10 +112,9 @@ with open("model_matching_gains", "rb") as inf:
     mm_gains = dill.load(inf)
 mm_ctrl = StaticController(mm_gains)
 
-# #Steer into lean controller
-# # TODO: Set the correct sil gains
-# sil_ctrl = VariableController(sil_gain_fun)
-
+#Steer into lean controller
+# TODO: set correct V_AVG
+sil_ctrl = VariableController(sil_gain_fun)
 
 # ###--------[SIMULATE
 # # first try, single speed
