371346191c63f717388bc2219ee8a429d9ae9f7b
kpasma <38067966+kpasma@users.noreply.github.com>
2024-03-05 12:49:19 +0100

    Theoretical speed eigenvalue plot now gives the speed-eigen of bicycle WITH sil contoller. As this is the one we actually measure during experiments

--- data_analysis/theoretical_speed_eigenvalue.py
+++ data_analysis/theoretical_speed_eigenvalue.py
@@ -1,7 +1,11 @@
 import numpy as np
 import inspect
 import dill
-import matplotlib.pyplot as plt
+
+# Steer into lean conroller
+SIL_AVG_SPEED = 5.5
+K_SIL_L = 2
+K_SIL_H = 0.7
 
 class VariableStateSpaceSystem:
     '''
@@ -41,6 +45,53 @@ class VariableStateSpaceSystem:
         if "D" not in self.mat:
             self.mat["D"] = np.zeros((self.mat["C"].shape[0], self.mat["B"].shape[1]))       
 
+class VariableController:
+    def __init__(self,fun: dict):
+        self.gain_fun = fun
+        self.gain = {}
+
+    def __str__(self):
+        str = ""
+        for key, value in self.gain.items():
+            str = str + key + f": {value}\n"
+        return str
+
+    def calc_gain(self, var):
+        for key,val in self.gain_fun.items():
+            if len(inspect.getfullargspec(val).args): #check if the gain matrix requires inputs
+                self.gain[key] = val(var)
+            else:
+                self.gain[key] = val()
+
+def sil_gain_F_fun(speed):
+    '''
+    Feedback part of the SiL controller.
+    See Schwab et al., 'Some Recent Developments in Bicycle Dynamics and Control', 2008
+    Dimensions: A-4x4, B-4x2
+    State vector: [phi, delta, dphi, ddelta],
+    Input vector: [Tphi, Tdelta]
+    TODO: remove magic numbers (2,4) and [2][1]?
+    '''
+    gain = np.zeros((2,4))
+    if speed < SIL_AVG_SPEED:
+        gain[1][2] = K_SIL_L*(SIL_AVG_SPEED - speed) # *dphi
+    else:
+        gain[1][0] = K_SIL_H*(speed - SIL_AVG_SPEED) # *phi
+    return gain
+
+def sil_gain_G_fun():
+    '''
+    Feedforward part of the SiL controller.
+    As the control is only dependend on the states,
+    there will be no control reacting to the external
+    inputs. For theory this means that the feedforward
+    part is identity.
+    Matrix having appropriate dimensions.
+    Dimensions: B-4x2
+    Input vector: [Tphi, Tdelta]
+    TODO: remove magic numbers 2?
+    '''
+    return np.eye(2)
 
 def get_eigen_vs_speed(bike_plant_file,plant_type,start,stop,step):
     '''
@@ -60,14 +111,18 @@ def get_eigen_vs_speed(bike_plant_file,plant_type,start,stop,step):
         sys_mtrx = dill.load(inf)
     plant = VariableStateSpaceSystem(sys_mtrx[plant_type])
 
+    # Create SiL controller
+    sil_ctrl = VariableController({"F": sil_gain_F_fun, "G": sil_gain_G_fun})
+
     speedrange = np.linspace(start , stop , num=int(1 + (stop-start)/step))
     eigenvals = [None for k in range(len(speedrange))]
 
     for idx, speed in enumerate(speedrange):
         # calculate speed depenend matrices
         plant.calc_mtrx(speed)
+        sil_ctrl.calc_gain(speed)
         # calculate eigenvalues
-        eigenvals[idx] = np.linalg.eigvals(plant.mat["A"]) # plant-> dx = Ax + Bu
+        eigenvals[idx] = np.linalg.eigvals(plant.mat["A"] + plant.mat["B"]@sil_ctrl.gain["F"]) # plant-> dx = Ax + Bu
 
     # Reorganize results for plotting
     eigenvals = {
