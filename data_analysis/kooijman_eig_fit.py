2fadc857fb049881ad7ade8f751d22ebe0e74ee1
kpasma <38067966+kpasma@users.noreply.github.com>
2024-04-10 12:20:29 +0200

    analysed the data from the april 9th normal bike eigenvalue identification. IN ADDITION, added a high pass filter to remove the low drift that is continuously present in the bike for some reasron

--- data_analysis/kooijman_eig_fit.py
+++ data_analysis/kooijman_eig_fit.py
@@ -48,8 +48,8 @@ def extract_data(full_path,start,stop,time_step,vars2extract):
         if(TO_ANALYSE == "raw"):
             signal = value
         elif(TO_ANALYSE == "filtered"):
-            signal = filt.butter_static(BUTTER_ORDER, BUTTER_CUT_OFF, value, fs=1/time_step)
-        
+            signal = filt.first_order_hp(HIGH_PASS_Wc_FREQ,value,fs=1/time_step)
+            # signal = filt.butter_static(BUTTER_ORDER, BUTTER_CUT_OFF, value, fs=1/time_step)
         extraction[key] = signal[start:stop]
 
     time = np.linspace(0,time_step*(len(extraction[key])-1),len(extraction[key]))
@@ -96,22 +96,23 @@ def plot_uncut_data(path,file,vars2extract):
 #=====START=====#
 #---[Constants
 PATH = "..\\teensy\\logs\\"
-TO_ANALYSE = "raw" # "raw" or "filtered"
-BUTTER_ORDER = 2
-BUTTER_CUT_OFF = 20
+TO_ANALYSE = "filtered" # "raw" or "filtered"
+# BUTTER_ORDER = 2
+# BUTTER_CUT_OFF = 10
+HIGH_PASS_Wc_FREQ = 1
 TIME_STEP = 0.01
-PHASE = "cut_data" # "cut_data" or "calculate_eig"
+PHASE = "calculate_eig" # "cut_data" or "calculate_eig"
 VISUAL_CHECK_FIT = False # If true, show graph for visually checking the kooijman function fit
 MAX_FUN_EVAL = 5000
 
 #Theoretical model parameters
 PLANT_TYPE = "plant" #"plant" or "reference"
-SPEED_DEP_MODEL_FILE = "..\\model matching gain calculation\\bike_and_ref_variable_dependend_system_matrices"
+SPEED_DEP_MODEL_FILE = "..\\model matching gain calculation\\bike_and_ref_variable_dependend_system_matrices_measured_parameters_corrected"
 SPEED_START = 0
 SPEED_STOP = 10
 SPEED_STEP = 0.01
 SIL_PARAMETERS = {
-    'avg_speed' : 5.5,
+    'avg_speed' : 6.5,
     'L_gain': 2,
     'H_gain': 0.7
 }
@@ -127,29 +128,68 @@ vars2extract = {
         # "y_acceleration": [],
     }
 log_files = [
-    ("eigenvaltest_16kph_6bar_error_in_sil.log", (0,0)),
+    ("eigen_normal_sil6.5n2_18kph.log", [(0,0)])
 ]
-experiments = [ #file,speed[km/h],start&end in file, initial values)
-    # ("eigenvaltest_08kph_6bar_error_in_sil.log", 8, (4527,4626), (-1.0, 3.0, -1.0, 1.0, 1.0)), #Questionable
-    # ("eigenvaltest_08kph_6bar_error_in_sil.log", 8, (5431,5533), (-1.0, 3.0, -1.0, 1.0, 1.0)), #Questionable
-    ("eigenvaltest_08kph_6bar_error_in_sil.log", 8, (6373,6485), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    ("eigenvaltest_10kph_6bar_error_in_sil.log", 10, (3958,4060), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    ("eigenvaltest_10kph_6bar_error_in_sil.log", 10, (4906,4995), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    # ("eigenvaltest_10kph_6bar_error_in_sil.log", 10, (5957,6240), (-1.0, 3.0, -1.0, 1.0, 1.0)), #Questionable
-    ("eigenvaltest_12kph_6bar_error_in_sil.log", 12, (3971,4080), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    ("eigenvaltest_12kph_6bar_error_in_sil.log", 12, (4755,4885), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    # ("eigenvaltest_12kph_6bar_error_in_sil.log", 12, (5671,5895), (-1.0, 3.0, -1.0, 1.0, 1.0)), #Questionable
-    ("eigenvaltest_14kph_6bar_error_in_sil.log", 14, (4355,4441), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    ("eigenvaltest_14kph_6bar_error_in_sil.log", 14, (5226,5347), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    # ("eigenvaltest_14kph_6bar_error_in_sil.log", 14, (6038,6274), (-1.0, 3.0, -1.0, 1.0, 1.0)), #Questionable
-    # ("eigenvaltest_16kph_6bar_error_in_sil.log", 16, (4087,4277), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    # ("eigenvaltest_16kph_6bar_error_in_sil.log", 16, (4911,5174), (-1.0, 3.0, -1.0, 1.0, 1.0)), #Questionable
-    ("eigenvaltest_16kph_6bar_error_in_sil.log", 16, (5976,6073), (-1.0, 3.0, -1.0, 1.0, 1.0)), 
-    ("eigenvaltest_16kph_6bar_error_in_sil.log", 16, (7002,7097), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    ("eigenvaltest_18kph_6bar_error_in_sil.log", 18, (6707,6866), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    ("eigenvaltest_18kph_6bar_error_in_sil.log", 18, (7756,7902), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-    # ("eigenvaltest_18kph_6bar_error_in_sil.log", 18, (8753,8982), (-1.0, 3.0, -1.0, 1.0, 1.0)), #Questionable
-    ("eigenvaltest_18kph_6bar_error_in_sil.log", 18, (9749,10013), (-1.0, 3.0, -1.0, 1.0, 1.0))
+experiments = [ #file,speed[km/h],start&end in file, initial values
+    ("eigen_normal_sil6.5n2_5.4kph.log", 5.4, (4486,4775), (-3.0, 5.5, 0.3, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_5.4kph.log", 5.4, (5420,5668), (-3.0, 5.5, 0.35, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_5.4kph.log", 5.4, (6325,6499), (-3.0, 5.5, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_5.4kph.log", 5.4, (7349,7532), (-3.0, 5.5, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_5.4kph.log", 5.4, (8984,9214), (-3.0, 5.5, 0.3, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_5.4kph.log", 5.4, (9750,9925), (-3.0, 5.5, -0.25, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_5.4kph.log", 5.4, (10600,10845), (-3.0, 5.5, 0.2, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_7.2kph.log", 7.2, (9870,10039), (-3.0, 7.5, -0.25, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_7.2kph.log", 7.2, (11024,11137), (-3.0, 7.5, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_7.2kph.log", 7.2, (12689,12854), (-3.0, 7.5, -0.25, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_7.2kph.log", 7.2, (13773,13934), (-3.0, 7.5, -0.35, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_7.2kph.log", 7.2, (14886,15052), (-3.0, 7.5, 0.2, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_9kph.log", 9, (2121,2273), (-2.5, 8.5, -0.3, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_9kph.log", 9, (3002,3281), (-2.5, 8.5, 0.2, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_9kph.log", 9, (8673,8765), (-2.5, 8.5, -0.3, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_9kph.log", 9, (9613,9940), (-2.5, 8.5, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_9kph.log", 9, (11049,11416), (-2.5, 8.5, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_9kph.log", 9, (12487,12695), (-2.5, 8.5, 0.3, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_10.8kph.log", 10.8, (1573,1760), (-2.5, 9.5, 0.25, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_10.8kph.log", 10.8, (2601,2750), (-2.5, 9.5, 0.25, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_10.8kph.log", 10.8, (3577,3815), (-2.5, 9.5, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_10.8kph.log", 10.8, (5682,5923), (-2.5, 9.5, 0.25, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_10.8kph.log", 10.8, (6527,6772), (-2.5, 9.5, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_10.8kph.log", 10.8, (7471,7705), (-2.5, 9.5, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_10.8kph.log", 10.8, (8371,8581), (-2.5, 9.5, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_10.8kph.log", 10.8, (9424,9585), (-2.5, 9.5, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_10.8kph.log", 10.8, (10187,10470), (-2.5, 9.5, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_12.6kph.log", 12.6, (2028,2246), (-2.5, 10.0, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_12.6kph.log", 12.6, (3053,3175), (-2.5, 10.0, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_12.6kph.log", 12.6, (3983,4231), (-2.5, 10.0, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_12.6kph.log", 12.6, (4982,5191), (-2.5, 10.0, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_12.6kph.log", 12.6, (6881,7073), (-2.5, 10.0, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_12.6kph.log", 12.6, (7898,8076), (-2.5, 10.0, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_12.6kph.log", 12.6, (8749,8970), (-2.5, 10.0, 0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_12.6kph.log", 12.6, (9733,9992), (-2.5, 10.0, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_12.6kph.log", 12.6, (10872,11169), (-2.5, 10.0, 0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_14.4kph.log", 14.4, (1410,1538), (-2.3, 10.0, 0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_14.4kph.log", 14.4, (2385,2677), (-2.3, 10.0, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_14.4kph.log", 14.4, (3400,3610), (-2.3, 10.0, 0.3, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_14.4kph.log", 14.4, (4352,4573), (-2.3, 10.0, -0.6, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_14.4kph.log", 14.4, (5276,5581), (-2.3, 10.0, -0.6, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_14.4kph.log", 14.4, (6311,6560), (-2.3, 10.0, -0.6, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_14.4kph.log", 14.4, (7347,7559), (-2.3, 10.0, -0.6, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_14.4kph.log", 14.4, (8423,8653), (-2.3, 10.0, -0.6, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_16.2kph.log", 16.2, (1576,1828), (-2.3, 10.0, -0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_16.2kph.log", 16.2, (2529,2695), (-2.3, 10.0, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_16.2kph.log", 16.2, (5041,5191), (-2.3, 10.0, 0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_16.2kph.log", 16.2, (5796,6034), (-2.3, 10.0, -0.6, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_16.2kph.log", 16.2, (8035,8212), (-2.3, 10.0, 0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_16.2kph.log", 16.2, (13604,13800), (-2.3, 10.0, -0.6, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_18kph.log", 18, (1964,2310), (-2.0, 9.5, 0.4, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_18kph.log", 18, (2917,3221), (-2.0, 9.5, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_18kph.log", 18, (3831,4007), (-2.0, 9.5, 0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_18kph.log", 18, (4594,4874), (-2.0, 9.5, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_18kph.log", 18, (5549,5679), (-2.0, 9.5, 0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_18kph.log", 18, (6326,6542), (-2.0, 9.5, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_18kph.log", 18, (7060,7310), (-2.0, 9.5, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_18kph.log", 18, (12196,12360), (-2.0, 9.5, -0.5, 1.0, 1.0)),
+    ("eigen_normal_sil6.5n2_18kph.log", 18, (12961,13073), (-2.0, 9.5, -0.5, 1.0, 1.0)),
 ]
 
 
@@ -167,14 +207,16 @@ elif(PHASE == "cut_data"):
     for foo in log_files:
         log, start_stop = foo
         ax = plot_uncut_data(PATH,log,vars2extract)
-        ax.axvline(start_stop[0])
-        ax.axvline(start_stop[1])
+        for trial in start_stop:
+            ax.axvline(trial[0])
+            ax.axvline(trial[1])
         plt.show()
 
 
 # -- [ Frist real test (but with false sil controller)
-# ("eigenvaltest_08kph_6bar_error_in_sil.log", 8, (4527,4626), (-1.0, 3.0, -1.0, 1.0, 1.0)),
-# ("eigenvaltest_08kph_6bar_error_in_sil.log", 8, (5431,5533), (-1.0, 3.0, -1.0, 1.0, 1.0)),
+# SPEED_DEP_MODEL_FILE = "..\\model matching gain calculation\\bike_and_ref_variable_dependend_system_matrices"
+# # ("eigenvaltest_08kph_6bar_error_in_sil.log", 8, (4527,4626), (-1.0, 3.0, -1.0, 1.0, 1.0)), #Questionable
+# # ("eigenvaltest_08kph_6bar_error_in_sil.log", 8, (5431,5533), (-1.0, 3.0, -1.0, 1.0, 1.0)), #Questionable
 # ("eigenvaltest_08kph_6bar_error_in_sil.log", 8, (6373,6485), (-1.0, 3.0, -1.0, 1.0, 1.0)),
 # ("eigenvaltest_10kph_6bar_error_in_sil.log", 10, (3958,4060), (-1.0, 3.0, -1.0, 1.0, 1.0)),
 # ("eigenvaltest_10kph_6bar_error_in_sil.log", 10, (4906,4995), (-1.0, 3.0, -1.0, 1.0, 1.0)),
