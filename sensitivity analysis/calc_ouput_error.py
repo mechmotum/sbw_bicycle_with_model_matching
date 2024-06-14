8a4fbcbd508cb7fed38b98f93aba9aeff12ed6cb
kpasma <38067966+kpasma@users.noreply.github.com>
2024-06-14 13:14:53 +0200

    Used the Average instead of the total error. In this way, it is more comparable if you change the range of interest, or change the number of eigenvalues or bode input to output plots --> average over the number of points in one plot + average over the number of lines/plots (so 8 eigenvalues lines and 4 bode plots

--- sensitivity analysis/calc_ouput_error.py
+++ sensitivity analysis/calc_ouput_error.py
@@ -3,11 +3,11 @@ import numpy as np
 def output_error_eig(eig_ctrld_plant, eig_ref):
     error = np.sum(
        np.abs((eig_ctrld_plant['real'] - eig_ref['real']))
-    )
+    )/eig_ctrld_plant['real'].shape[0]
     error = error +  np.sum(
         np.abs((eig_ctrld_plant['imag'] - eig_ref['imag']))
-    )
-    return error
+    )/eig_ctrld_plant['imag'].shape[0]
+    return (error / (eig_ctrld_plant['real'].shape[1] + eig_ctrld_plant['imag'].shape[1]))
 
 def output_error_bode(bode_ctrld_plant, bode_ref):
     error = 0
@@ -15,5 +15,5 @@ def output_error_bode(bode_ctrld_plant, bode_ref):
         for out_value in range(bode_ctrld_plant.shape[1]):
             error = error + np.sum(
                 np.abs(bode_ref[in_value,out_value,:] - bode_ctrld_plant[in_value,out_value,:])
-            )
-    return error
+            )/len(bode_ref[in_value,out_value,:])
+    return (error / (bode_ctrld_plant.shape[0]*bode_ctrld_plant.shape[1]))
