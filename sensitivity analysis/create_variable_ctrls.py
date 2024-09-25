ab91c01cd481a1cc2e64eaa3d7c7a34b74ae48ac
kpasma <38067966+kpasma@users.noreply.github.com>
2024-09-25 17:41:53 +0200

    Cleaned up and commented create_variable_ctrls.py

--- sensitivity analysis/create_variable_ctrls.py
+++ sensitivity analysis/create_variable_ctrls.py
@@ -1,11 +1,39 @@
+'''
+___[ create_variable_ctrls.py ]___
+The feedback gain is dependend on the speed. Controller 
+notation therefore has to be coded such that they include 
+this speed dependency. This class is a container for the 
+controller, whos numerical vaues are dependent on a single 
+parameter.
+'''
 import inspect
 
 class VariableController:
+    '''
+    init:
+    initialize using a dictionairy that is structure as
+    key = gain function name, value = function to evaluate the
+    parametrized gain matrix
+
+    calc_gain:
+    Takes in a variable needed to calculate the 
+    parametrized gain matrix, and uses the member dictionairy 
+    gain_fun (initialized in init) to calculate the 
+    numarical gain matrix values.
+    NOTE: For now, all matrices have to be dependend on the 
+    same variable. (in this case it is 
+    speed)
+    '''
     def __init__(self,fun: dict):
         self.gain_fun = fun
         self.gain = {}
 
     def __str__(self):
+        '''
+        Function that creates a string object 
+        of all the calculated numerical matrices.
+        Usefull for debugging
+        '''
         str = ""
         for key, value in self.gain.items():
             str = str + key + f": {value}\n"
